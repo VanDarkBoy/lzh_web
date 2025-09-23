123143f639764817075d27c19fcb13694d77da7f
VanDarkBoy <vandark63@gmail.com>
2025-09-23 14:49:53 +0800

    Add header navigation to get started page

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import Header from '../components/Header';
 import ContactForm from './ContactForm';
 
 export default function GetStartedPage() {
   return (
     <div className="min-h-screen bg-slate-950">
-      <ContactForm />
+      <Header />
+      <main className="pt-24 sm:pt-32">
+        <ContactForm />
+      </main>
     </div>
   );
 }
