4f11ebde6762e2f005045f9b17a9d6a8c9e460d1
Caster <496342351@qq.com>
2025-11-07 11:06:40 +0800

    ProjectCategories.tsx

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -2,16 +2,18 @@
 
 import Header from '../components/Header';
 import ContactForm from './ContactForm';
+import Footer from "@/app/components/Footer";
 import WhatAPP from '../components/WhatAPP';
 
 export default function GetStartedPage() {
-  return (
-    <div className="min-h-screen bg-slate-950">
-      <Header />
-      <main>
-        <ContactForm />
-      </main>
-      <WhatAPP />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-slate-950">
+            <Header/>
+            <main>
+                <ContactForm/>
+            </main>
+            <Footer/>
+            <WhatAPP/>
+        </div>
+    );
 }
